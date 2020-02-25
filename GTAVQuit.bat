4bcc917bf5a7f483675136642f0235e4c1b30fd1
Josef <josef733@hotmail.com>
2020-02-25 01:14:15 +0100

    Changes to connection
    Must provide your own name and ssid for connection

--- GTAVQuit.bat
+++ GTAVQuit.bat
@@ -7,6 +7,6 @@ taskkill /IM GTA5.exe
 @echo When GTA5 has been closed, unpause
 timeout -1
 taskkill /IM RockstarService.exe
-netsh wlan connect name=Get-306F6C ssid=Get-306F6C
+netsh wlan connect name=**** ssid=****
 @echo CMD will close in 5 seconds...
 @timeout 5 > NUL
