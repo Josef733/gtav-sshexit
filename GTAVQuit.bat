45138c5eed2af7e5bf2ede1dba39def53f9f16a2
Josef <josef733@hotmail.com>
2020-03-29 01:06:48 +0100

    Force Close & Text
    GTA now force closes.
    Information is now cleaner to read.
    Works flawlessly.

--- GTAVQuit.bat
+++ GTAVQuit.bat
@@ -1,12 +1,13 @@
-netsh wlan disconnect
+@netsh wlan disconnect
+@echo Disconnecting from Internet...
 @echo.
 @echo When "Unable to connect to Rockstar Games Services" appears, unpause
-timeout -1
-taskkill /IM GTA5.exe
-@echo. 
+@timeout -1
+@taskkill /F /IM GTA5.exe /T
 @echo When GTA5 has been closed, unpause
-timeout -1
-taskkill /IM RockstarService.exe
-netsh wlan connect name=**** ssid=****
-@echo CMD will close in 5 seconds...
+@timeout -1
+@echo Reconnecting to Internet...
+@netsh wlan connect name=Get-306F6C ssid=Get-306F6C
+@echo.
+@echo Reconnected, CMD will close in 5 seconds...
 @timeout 5 > NUL
